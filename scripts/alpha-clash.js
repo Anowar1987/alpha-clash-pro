22b8742040e373c625a04fdecc5eb28619286ee2
Anowar1987 <anowarhossen.iim@gmail.com>
2024-02-15 18:35:07 +0600

    added keyboard function

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,7 +8,21 @@
 //     // console.log(playgroundSection);
 // }
 
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+
+}
+
 function play(){
     hideElementByID('home-screen');
     showElementByID('play-ground');
+    continueGame();
 }
