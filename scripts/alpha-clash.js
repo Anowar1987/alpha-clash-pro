ddfe915eb3e0c5139132a1b5d3b852d4fc4e3c04
Anowar1987 <anowarhossen.iim@gmail.com>
2024-02-16 23:40:54 +0600

    updated

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -15,12 +15,15 @@ function handleKeyboardBtuuonPress(event){
     // get expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
-    const expetedAlphabet = currentAlphabet.toLocaleLowerCase()
-    console.log(playerPressed, expetedAlphabet);
+    const expetedAlphabet = currentAlphabet.toLowerCase()
+    // console.log(playerPressed, expetedAlphabet);
 
-    // check 
+    // check right or wrong key pressed
     if(playerPressed === expetedAlphabet){
         console.log('You got a point');
+        console.log('you have pressed correctly', expetedAlphabet);
+        removeBackgroundColorById(expetedAlphabet);
+        continueGame();
     }
     else{
         console.log('You missed a point. You lost a life');
@@ -30,7 +33,7 @@ document.addEventListener('keyup', handleKeyboardBtuuonPress);
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
 
     // set randomly generated alphabet to the screen (show it)
     const currentAlphabetElement = document.getElementById('current-alphabet');
